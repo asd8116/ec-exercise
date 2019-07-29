d57dfa45e5707b899ff5fd9582200b329f90ebd0
wanaka <asd8116@gmail.com>
2019-07-29 19:59:49 +0800

    Add Date Filter & modify Sidebar vue

--- src/main.js
+++ src/main.js
@@ -9,12 +9,14 @@ import App from './App.vue'
 import router from './router'
 import './bus'
 import currencyFilter from './filters/currency'
+import dateFilter from './filters/date'
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
 
 Vue.component('Loading', Loading)
 Vue.filter('currency', currencyFilter)
+Vue.filter('date', dateFilter)
 
 axios.defaults.withCredentials = true
 
