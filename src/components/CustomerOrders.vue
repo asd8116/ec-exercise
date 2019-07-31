39095d14a4e1cc3a6a05b2439b6a3bb8b93409d5
wanaka <asd8116@gmail.com>
2019-07-31 15:00:39 +0800

    Add Cart views and action

--- src/components/CustomerOrders.vue
+++ src/components/CustomerOrders.vue
@@ -19,26 +19,111 @@
             </div>
           </div>
           <div class="card-footer d-flex">
-            <button type="button" class="btn btn-outline-secondary btn-sm">
-              <i class="fas fa-spinner fa-spin"></i>
+            <button type="button" class="btn btn-outline-secondary btn-sm" @click="getProduct(item.id)">
+              <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
               查看更多
             </button>
-            <button type="button" class="btn btn-outline-danger btn-sm ml-auto">
-              <i class="fas fa-spinner fa-spin"></i>
+            <button type="button" class="btn btn-outline-danger btn-sm ml-auto" @click="addtoCart(item.id)">
+              <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
               加到購物車
             </button>
           </div>
         </div>
       </div>
     </div>
+
+    <div class="modal fade" id="productModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">{{ product.title }}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <img :src="product.image" class="img-fluid" alt />
+            <blockquote class="blockquote mt-3">
+              <p class="mb-0">{{ product.content }}</p>
+              <footer class="blockquote-footer text-right">{{ product.description }}</footer>
+            </blockquote>
+            <div class="d-flex justify-content-between align-items-baseline">
+              <div class="h4" v-if="!product.price">{{ product.origin_price }} 元</div>
+              <del class="h6" v-if="product.price">原價 {{ product.origin_price }} 元</del>
+              <div class="h4" v-if="product.price">現在只要 {{ product.price }} 元</div>
+            </div>
+            <select name class="form-control mt-3" v-model="product.num">
+              <option :value="num" v-for="num in 10" :key="num">選購 {{num}} {{product.unit}}</option>
+            </select>
+          </div>
+          <div class="modal-footer">
+            <div class="text-muted text-nowrap mr-3">
+              小計
+              <strong>{{ product.num * product.price }}</strong> 元
+            </div>
+            <button type="button" class="btn btn-primary" @click="addtoCart(product.id, product.num)">
+              <i class="fas fa-spinner fa-spin" v-if="product.id === status.loadingItem"></i>
+              加到購物車
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="my-5 row justify-content-center">
+      <div class="col-md-6">
+        <table class="table">
+          <thead>
+            <th></th>
+            <th>品名</th>
+            <th>數量</th>
+            <th>單價</th>
+          </thead>
+          <tbody v-if="cart.carts">
+            <tr v-for="item in cart.carts" :key="item.id">
+              <td class="align-middle">
+                <button type="button" class="btn btn-outline-danger btn-sm">
+                  <i class="far fa-trash-alt"></i>
+                </button>
+              </td>
+              <td class="align-middle">
+                {{ item.product.title }}
+                <!-- <div class="text-success" v-if="item.coupon">
+                  已套用優惠券
+                </div>-->
+              </td>
+              <td class="align-middle">{{ item.qty }}/{{ item.product.unit }}</td>
+              <td class="align-middle text-right">{{ item.final_total }}</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colspan="3" class="text-right">總計</td>
+              <td class="text-right">{{ cart.total }}</td>
+            </tr>
+            <!-- <tr v-if="cart.final_total">
+              <td colspan="3" class="text-right text-success">折扣價</td>
+              <td class="text-right text-success">{{ cart.final_total }}</td>
+            </tr>-->
+          </tfoot>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import $ from 'jquery'
+
 export default {
   data() {
     return {
       products: [],
+      product: {},
+      status: {
+        loadingItem: ''
+      },
+      cart: {},
       isLoading: false
     }
   },
@@ -53,10 +138,51 @@ export default {
         console.log(response)
         vm.isLoading = false
       })
+    },
+    getProduct(id) {
+      const vm = this
+      const url = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_CUSTOMPATH}/product/${id}`
+
+      vm.status.loadingItem = id
+      this.$http.get(url).then(response => {
+        vm.product = response.data.product
+        $('#productModal').modal('show')
+        console.log(response)
+        vm.status.loadingItem = ''
+      })
+    },
+    addtoCart(id, qty = 1) {
+      const vm = this
+      const url = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_CUSTOMPATH}/cart`
+
+      vm.status.loadingItem = id
+      const cart = {
+        product_id: id,
+        qty
+      }
+
+      this.$http.post(url, { data: cart }).then(response => {
+        console.log(response)
+        vm.status.loadingItem = ''
+        vm.getCart()
+        $('#productModal').modal('hide')
+      })
+    },
+    getCart() {
+      const vm = this
+      const url = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_CUSTOMPATH}/cart`
+
+      vm.isLoading = true
+      this.$http.get(url).then(response => {
+        vm.cart = response.data.data
+        console.log(response)
+        vm.isLoading = false
+      })
     }
   },
   created() {
     this.getProducts()
+    this.getCart()
   }
 }
 </script>
